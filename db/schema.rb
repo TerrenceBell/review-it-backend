4589fba79957910b93af4587a177cb71dc510854
TerrenceBell <terrencekbell@gmail.com>
2020-06-27 22:23:30 -0500

    adding relationships and migration for review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_27_034235) do
+ActiveRecord::Schema.define(version: 2020_06_28_032014) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -21,4 +21,15 @@ ActiveRecord::Schema.define(version: 2020_06_27_034235) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "rating"
+    t.string "title"
+    t.text "content"
+    t.integer "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_reviews_on_item_id"
+  end
+
+  add_foreign_key "reviews", "items"
 end
